42a7884e2672c071610474ffcb62ee5c34ae95ad
yangxiaohei <yangxiaohei321123@163.com>
2019-05-26 21:14:19 +0800

    刷题

---
+++ Solutions/progs/replace_space_with_no_extra_space.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void replace_space(char *s, size_t len)
+{
+    int nspace = 0;
+    for (int i = 0; i < len; i++)
+        if (s[i] == ' ')
+            nspace++;
+    size_t newlen = len + 2 * nspace + 1;
+    char *oldp = s, *newp = s;
+    oldp += len;
+    newp += newlen;
+    *newp-- = 0;
+
+    while (len--) {
+        if (*oldp == ' ') {
+            *newp-- = '0';
+            *newp-- = '2';
+            *newp-- = '%';
+            oldp--;
+        } else
+            *newp-- = *oldp--;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char s[128] = "1 2 3 4 ";
+    replace_space(s, strlen(s));
+    printf("%s\n", s);
+}
