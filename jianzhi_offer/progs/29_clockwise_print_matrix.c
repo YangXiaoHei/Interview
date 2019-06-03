7bda007f78914d1e61aa07713e94e1e1a44bff12
yangxiaohei <yangxiaohei321123@163.com>
2019-06-03 23:22:04 +0800

    循环打印矩阵

--- jianzhi_offer/progs/29_clockwise_print_matrix.c
+++ jianzhi_offer/progs/29_clockwise_print_matrix.c
@@ -19,22 +19,28 @@ int vector[][2] = {
 
 void clockwise_print_matrix(int *matrix, int row, int col)
 {
+    if (!matrix || row <= 0 || col <= 0) return;
+
     bitset_init(a, 10000);
 
     int dir = right;
     int i = 0, j = 0;
     int next_i, next_j;
+
     while (!bitset_test(a, i * col + j) && i >= 0 && i < row && j >= 0 && j < col) {
+
         bitset_set(a, i * col + j);
-    
         printf("%-3d", matrix[i * col + j]);
 
         next_i = i + vector[dir][0];
         next_j = j + vector[dir][1];
-        if (next_i < 0 || next_i >= row || next_j < 0 || next_j >= col || bitset_test(a, next_i * col + next_j)) {
+
+        if (next_i < 0 || next_i >= row || 
+            next_j < 0 || next_j >= col || 
+            bitset_test(a, next_i * col + next_j)) {
             dir = (dir + 1) % 4;
-            i = i + vector[dir][0];
-            j = j + vector[dir][1];
+            i += vector[dir][0];
+            j += vector[dir][1];
         } else {
             i = next_i;
             j = next_j;
