8744aca154fe40d22c6db0f3d7f9958e3c1dc647
yangxiaohei <yangxiaohei321123@163.com>
2019-07-14 19:40:40 +0800

    叼叼的二叉树非递归遍历方式，时间复杂度 On, 空间复杂度 O1

---
+++ code_guide/progs/035_tree_in_traverse_best.c
@@ -0,0 +1,36 @@
+#include "ds.h"
+
+void tree_in_traverse_best(treenode *root)
+{
+    if (!root) {
+        printf("null\n");
+        return;
+    }
+
+    treenode *cur1 = root, *cur2 = NULL;
+    while (cur1) {
+        cur2 = cur1->left;
+        if (cur2) {
+            while (cur2->right && cur2->right != cur1)
+                cur2 = cur2->right;
+
+            if (!cur2->right) {
+                cur2->right = cur1;
+                cur1 = cur1->left;
+                continue;
+            }
+
+            cur2->right = NULL;
+        }
+        printf("%-3ld", cur1->val);
+        cur1 = cur1->right;
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+    treenode *root = bst_create(10);
+    tree_draw(root);
+    tree_in_traverse_best(root);
+}
