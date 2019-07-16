70c7989d1c1f13425c36ef0335f0d993008528fa
yangxiaohei <yangxiaohei321123@163.com>
2019-07-16 21:56:34 +0800

    二叉树按层次并且以 zigzag 型打印

---
+++ code_guide/progs/039_tree_level_zigzag_print.c
@@ -0,0 +1,48 @@
+#include "ds.h"
+
+void tree_level_zigzag_print(treenode *root)
+{
+    if (!root) {
+        printf("null\n");
+        return;
+    }
+
+    stack *fleft = stack_create();
+    stack *fright = stack_create();
+
+#define PUSH(s, x) stack_push(s, (long)x)
+#define POP(s) ((treenode *)stack_pop(s))
+
+    int i = 0;
+    PUSH(fleft, root);
+    while (!stack_empty(fleft) || !stack_empty(fright)) {
+        if (i++ & 1) {
+            while (!stack_empty(fright)) {
+                root = POP(fright);
+                printf("%-3ld", root->val);
+                if (root->right)
+                    PUSH(fleft, root->right);
+                if (root->left)
+                    PUSH(fleft, root->left);
+            }
+            printf("\n");
+        } else {
+            while (!stack_empty(fleft)) {
+                root = POP(fleft);
+                printf("%-3ld", root->val);
+                if (root->left)
+                    PUSH(fright, root->left);
+                if (root->right)
+                    PUSH(fright, root->right);
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    treenode *root = bst_create(10);
+    tree_draw(root);
+    tree_level_zigzag_print(root);
+}
