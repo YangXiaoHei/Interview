71bb006849b899386985582736245e4e193504fe
yangxiaohei <yangxiaohei321123@163.com>
2019-07-28 02:32:34 +0800

    矩阵最小路径和 - dp - 非压缩空间

--- code_guide/progs/056_dp_matrix_min_path_sum.c
+++ code_guide/progs/056_dp_matrix_min_path_sum.c
@@ -1,48 +1,63 @@
 #include "ds.h"
 
-long matrix_min_path_sum(long *m, int r, int c)
+int  min(int a, int c)
 {
-    long *dp = malloc(sizeof(long) * (r * c));
-    if (!dp) exit(1);
-    memset(dp, 0, sizeof(long) * (r * c));
+    return a < c ? a : c;
+}
 
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            long up = 0, left = 0;
-            if (i > 0)
-                up = dp[(i - 1) * c + j];
-            if (j > 0)
-                left = dp[i * c + j - 1];
-            dp[i * c + j] = (up < left ? up : left) + m[i * c + j];
-        }
-    }
+int max_sum_path(int *m, int r, int c)
+{
+    int *dp = malloc(sizeof(int) * r * c);
+    memset(dp, 0, sizeof(int) * r * c);
 
-    // 打印出该条路径
+    dp[0 * c + 0] = m[0 * c + 0];
+    for (int i = 1; i < r; i++)
+        dp[i * c + 0] = m[i * c + 0] + dp[(i - 1) * c + 0];
+
+    for (int i = 1; i < c; i++)
+        dp[0 * c + i] = m[0 * c + i] + dp[0 * c + i - 1];
+
+    for (int i = 1; i < r; i++) 
+        for (int j = 1; j < c; j++) 
+            dp[i * c + j] = m[i * c + j] + min(dp[(i - 1) * c + j], dp[i * c + j - 1]);
+
+    int *path = malloc(sizeof(int) * (r + c - 1));
+    memset(path, 0, sizeof(int) * (r + c - 1));
     int i = r - 1, j = c - 1;
-    while (i != 0 || j != 0) {
-        printf("%ld\n", m[i * c + j]);
-        int up = 2147483647, left = 2147483647;
+    int idx = r + c - 2;
+    while (idx >= 0) {
+        int up = 2147483647; 
+        int left = 2147483647; 
 
-        if (i > 0)
+        if (i >= 0 && j >= 0) {
             up = dp[(i - 1) * c + j];
-
-        if (j > 0)
+            left = dp[i * c + j - 1];
+        } else if (j < 0 && i < 0)
+            break;
+        else if (j < 0)
+            up = dp[(i - 1) * c + j];
+        else
             left = dp[i * c + j - 1];
 
-        if (up > left)
-            j--;
-        else
+        path[idx--] = m[i * c + j];
+        if (min(up, left) == up)
             i--;
+        else
+            j--;
     }
-    long res = dp[(r - 1) * c + c - 1];
+
+    printArray(path, r + c - 1);
+    free(path);
+
+    int res = dp[(r - 1) * c + c - 1];
     free(dp);
     return res;
 }
 
 int main(int argc, char *argv[])
 {
-    long *m = matrix_gen(7, 8);
-    matrix_print(m, 7, 8);
-    long sum = matrix_min_path_sum(m, 7, 8);
-    printf("min path sum %ld\n", sum);
+    int *m = matrix_gen_(4, 5);
+    matrix_print_(m, 4, 5);
+    int sum = max_sum_path(m, 4, 5);
+    printf("sum = %d\n", sum);
 }
