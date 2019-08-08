104a45d1154778cf45245f9298acedd2c6396e4e
yangxiaohei <yangxiaohei321123@163.com>
2019-08-08 20:55:31 +0800

    将字符串中的指定字符串替换成另一个指定字符串... 需要很细致

---
+++ code_guide/progs/078_string_replace.c
@@ -0,0 +1,76 @@
+#include "ds.h"
+
+char * replace(const char *str, const char *from, const char *to, char *buf, int buflen)
+{
+    int match = 0;
+    int len = strlen(str);
+    int flen = strlen(from);
+    int tlen = strlen(to);
+    int count = 0;
+    snprintf(buf, buflen, "%s", str);
+    for (int i = 0; i < len; i++) {
+        if (buf[i] == from[match]) {
+            if (++match == flen) {
+                count++;
+                int j = i;
+                while (match--) 
+                    buf[j--] = 0;
+                match = 0;
+            }
+        } else
+            match = 0;
+    } 
+
+    // 不会有任何替换发生
+    if (count == 0)
+        return buf;
+
+    int total_len = 0;
+    if (flen > tlen) {
+        if (buflen < len) {
+            printf("underflow\n");
+            return buf;
+        }
+
+        total_len = len - count * (flen - tlen);
+        for (int i = 0, j = 0; j < total_len;) {
+            if (buf[i]) {
+               buf[j++] = buf[i++];
+            } else {
+                while (i < len && !buf[i]) i++;
+                for (char *p = (char *)to; *p; p++)
+                    buf[j++] = *p;
+            } 
+        }
+    } else {
+        total_len = len + count * (tlen - flen);
+        if (total_len >= buflen) {
+            printf("overflow\n");
+            return buf;
+        }
+        for (int j = total_len - 1, i = len - 1; j >= 0;) {
+            if (buf[i]) {
+                buf[j--] = buf[i--];
+            } else {
+                while (i >= 0 && !buf[i]) i--;
+                for (int k = tlen - 1; k >= 0; k--)
+                    buf[j--] = to[k];
+            }
+        }
+    }
+    buf[total_len] = 0;
+    return buf;
+}
+
+int main(int argc, char *argv[])
+{
+    char buf[100];
+    int size = sizeof(buf);
+    printf("%s\n", replace("123456123456123", "123", "x", buf, size));
+    printf("%s\n", replace("123456123456123", "123", "xxxx", buf, size));
+    printf("%s\n", replace("123456123456123", "123", "xxx", buf, size));
+    printf("%s\n", replace("123456123456123", "123", "", buf, size));
+    printf("%s\n", replace("123456123456123", "123456123456123", "", buf, size));
+    printf("%s\n", replace("123456123456123", "123456123456123", "666", buf, size));
+    printf("%s\n", replace("123456123456123", "666", "999", buf, size));
+}
