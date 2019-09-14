9a1ef37116a39fd4fc8235b8d62082850ad39d10
yangxiaohei <yangxiaohei321123@163.com>
2019-09-14 18:16:06 +0800

    不用 + - * / 实现减法

---
+++ code_guide/progs/098_bitwise_sub.c
@@ -0,0 +1,29 @@
+#include "ds.h"
+
+int add(int a, int b)
+{
+    int c = 0;
+    while (b) {
+        c = a ^ b;
+        b = (a & b) << 1;
+        a = c;
+    }
+    return a;
+}
+
+int sub(int a, int b)
+{
+    return add(a, add(~b, 1));
+}
+
+int main()
+{
+    for (int i = -1000; i <= 1000; i++) {
+        for (int j = -1000; j <= 1000; j++) {
+            if (i - j != sub(i, j)) {
+                printf("i=%d j=%d\n", i, j);
+                return -1;
+            }
+        }
+    }
+}
