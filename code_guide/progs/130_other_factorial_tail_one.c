9d5db3b1c5bdd59fc29c5e9cc9fcd9aacba226fe
yangxiaohei <yangxiaohei321123@163.com>
2019-10-05 23:31:52 +0800

    阶乘末尾二进制位模式的1

---
+++ code_guide/progs/130_other_factorial_tail_one.c
@@ -0,0 +1,18 @@
+#include "ds.h"
+
+int tail_one(int n)
+{
+    int res = 0;
+    while (n) {
+        n /= 2;
+        res += n;
+    }
+    return res;
+}
+
+int main()
+{
+    for (int i = 0; i < 100; i++) {
+        printf("%d! tail one %d\n", i, tail_one(i));
+    }
+}
